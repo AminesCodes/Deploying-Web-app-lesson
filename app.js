9b10108263858dc74f188699d16a9a07bd91c3ab
AminesCodes <aminebensalem@pursuit.org>
2020-02-03 20:33:25 -0500

    created my backend

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const parser = require("body-parser");
+const cors = require("cors");
+const bookmarksRouter = require("./router/bookmarks");
+const usersRouter = require("./router/users");
+
+// instantiate express
+const app = express();
+const PORT = process.env.PORT || 8080
+
+// Middleware configuration
+
+// interprets key value pairs in URLs
+app.use(parser.urlencoded({ extended: true }));
+
+// converts a json string to the an object and attaches it to req.body
+app.use(parser.json());
+
+// cors allows connections from all domains
+app.use(cors());
+
+// redirect any requests to the homepage to bookmarks
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Welcome to the book-e API. The endpoints available are /api/users and /api/bookmarks"
+  })
+});
+
+// hands off requests on the '/api/bookmarks' route to the bookmarks Router
+app.use("/api/bookmarks/", bookmarksRouter);
+// hands off requests on the '/api/users' route to the users Router
+app.use("/api/users/", usersRouter);
+
+app.listen(PORT, () => console.log("They see me rollin...on port 8080..."));
